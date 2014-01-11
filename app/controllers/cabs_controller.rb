1589dcc187e857d303762ae443e0de9db5d6d240
SomethingWitty <tsai.stephen@gmail.com>
2014-01-10 20:09:40 -0800

    some db changes

--- app/controllers/cabs_controller.rb
+++ app/controllers/cabs_controller.rb
@@ -1,17 +1,22 @@
 class CabsController < ApplicationController
 	before_filter :check_lat_long, :only => [:create, :update]
   def index
+		params.require(:latitude)
+		params.require(:longitude)
+		params.permit(:limit, :radius)
+		@latitude = params[:latitude].to_f
+		@longitude = params[:longitude].to_f
 		@cabs = Cab.all
   end
 	def create
-		Cab.new(:latitude => params[:latitude], :longitude => params[:longitude]).save
+		Cab.create(:latitude => params[:latitude], :longitude => params[:longitude])
 		head :status => 200
 	end
 	def update	
 		params.require(:id)
 		cab = Cab.find(params[:id])
 		cab.update_attributes(:latitude => params[:latitude], :longitude => params[:longitude])
-		render :status => 200
+		head :status => 200
 	end
 	def destroy
 		params.require(:id)
